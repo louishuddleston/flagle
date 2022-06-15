055199a8f631550189f1c1ec9c2da65d6b0921a4
Ryan <ryanbarouki@gmail.com>
2022-06-15 10:52:47 +0100

    hopefully some non-invasive ads

--- src/components/GoogleAd.js
+++ src/components/GoogleAd.js
@@ -1,22 +1,34 @@
 import React, { useEffect } from 'react';
+import styled from 'styled-components';
 
+const StyledIns = styled.ins`
+    width: 320px !important;  
+    height: 100px !important;
+    @media (min-width:500px) { 
+        width: 468px; 
+        height: 60px; 
+    }
+    @media (min-width:800px) { 
+        width: 728px; 
+        height: 90px; 
+    }
+`;
 function GoogleAds(props) {
     const { currentPath } = props
     useEffect(() => {
         window.adsbygoogle = window.adsbygoogle || []
         window.adsbygoogle.push({
-            overlays: {bottom: true}
+        overlays: {bottom: true}
         })
     }, [])
     return (
         <div>
-            <ins className="adsbygoogle"
+            <StyledIns className="adsbygoogle"
                 style={{ display: "block" }}
                 data-ad-client="ca-pub-8549840240508631"
                 data-ad-slot={props.slot}
-                data-ad-format="horizontal"
-                data-full-width-responsive="true">
-            </ins>
+                >
+            </StyledIns>
         </div>
     );
 }
