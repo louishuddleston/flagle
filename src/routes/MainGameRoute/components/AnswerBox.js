9ec8dba0b5b200e5e376f5a893c24afb250690fa
Louis Huddleston <louishuddleston@gmail.com>
2023-11-08 16:58:05 +0000

    wider guess input

--- src/routes/MainGameRoute/components/AnswerBox.js
+++ src/routes/MainGameRoute/components/AnswerBox.js
@@ -6,11 +6,16 @@ const normalise = (value) => value.toUpperCase();
 const StyledSelect = styled(Select)`
   font-family: Courier, monospace;
   margin-bottom: 1rem;
-  min-width: 200px;
   color: #000;
   :hover {
     border-color: #123456;
   }
+`;
+
+const Container = styled.div`
+  width: 100%;
+  padding: 0 8px;
+  max-width: 376px;
 `;
 
 export const AnswerBox = ({
@@ -32,11 +37,13 @@ export const AnswerBox = ({
   );
 
   return (
-    <StyledSelect
-      options={sortedCountries}
-      onChange={handleSubmit}
-      placeholder="Guess the flag!"
-      isOptionDisabled={() => disabled}
-    />
+    <Container>
+      <StyledSelect
+        options={sortedCountries}
+        onChange={handleSubmit}
+        placeholder="Guess the flag!"
+        isOptionDisabled={() => disabled}
+      />
+    </Container>
   );
 };
