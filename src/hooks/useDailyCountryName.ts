efe34163778bdc2d595381829aa9ea17966060fe
Louis Huddleston <louishuddleston@gmail.com>
2023-11-09 13:41:32 +0000

    for testing temporarily randomise country on load or use query string

--- src/hooks/useDailyCountryName.ts
+++ src/hooks/useDailyCountryName.ts
@@ -1,23 +1,37 @@
-import { useMemo } from "react";
-import seedrandom from "seedrandom";
+import { useMemo } from 'react';
+import { useLocation } from 'react-router-dom';
+import seedrandom from 'seedrandom';
 
-import { useAllCountryNames } from "./useAllCountryNames";
-import { useDailySeed } from "./useDailySeed";
+import countryData from '../data/countries';
+import { useAllCountryNames } from './useAllCountryNames';
+import { useDailySeed } from './useDailySeed';
 
 export function useDailyCountryName(): string {
   const dailySeed = useDailySeed();
   const countryNames = useAllCountryNames();
 
-  return useMemo(() => {
-    const todaysCountry =
-      countryNames[
-        Math.floor(seedrandom.alea(dailySeed)() * countryNames.length)
-      ];
+  const location = useLocation();
+  const params = new URLSearchParams(location.search);
+  const code = params.get('code');
 
-    if (todaysCountry === "Russia") {
-      return "Ukraine";
+  return useMemo(() => {
+    let todaysCountry =
+      // countryNames[
+      //   // Math.floor(seedrandom.alea(dailySeed)() * countryNames.length)
+      //   // temporary use random instead of seedrandom
+      //   // Math.floor(Math.random() * countryNames.length)
+      //   country
+      // ];
+      countryNames[Math.floor(Math.random() * countryNames.length)];
+
+    Object.entries(countryData).forEach(([key, val]) => {
+      if (val.code === code) todaysCountry = key;
+    });
+
+    if (todaysCountry === 'Russia') {
+      return 'Ukraine';
     }
 
     return todaysCountry;
-  }, [dailySeed, countryNames]);
+  }, [dailySeed, countryNames, code]);
 }
