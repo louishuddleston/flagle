93c45e2e9aedb46a8c004df0d99320e7f6530b76
Louis Huddleston <louishuddleston@gmail.com>
2023-11-16 12:36:55 +0000

    set back to daily game

--- src/hooks/useDailyCountryName.ts
+++ src/hooks/useDailyCountryName.ts
@@ -1,8 +1,6 @@
 import { useMemo } from 'react';
-import { useLocation } from 'react-router-dom';
 import seedrandom from 'seedrandom';
 
-import countryData from '../data/countries';
 import { useAllCountryNames } from './useAllCountryNames';
 import { useDailySeed } from './useDailySeed';
 
@@ -10,28 +8,16 @@ export function useDailyCountryName(): string {
   const dailySeed = useDailySeed();
   const countryNames = useAllCountryNames();
 
-  const location = useLocation();
-  const params = new URLSearchParams(location.search);
-  const code = params.get('code');
-
   return useMemo(() => {
-    let todaysCountry =
-      // countryNames[
-      //   // Math.floor(seedrandom.alea(dailySeed)() * countryNames.length)
-      //   // temporary use random instead of seedrandom
-      //   // Math.floor(Math.random() * countryNames.length)
-      //   country
-      // ];
-      countryNames[Math.floor(Math.random() * countryNames.length)];
-
-    Object.entries(countryData).forEach(([key, val]) => {
-      if (val.code === code) todaysCountry = key;
-    });
+    const todaysCountry =
+      countryNames[
+        Math.floor(seedrandom.alea(dailySeed)() * countryNames.length)
+      ];
 
     if (todaysCountry === 'Russia') {
       return 'Ukraine';
     }
 
     return todaysCountry;
-  }, [dailySeed, countryNames, code]);
+  }, [dailySeed, countryNames]);
 }
